43815d8ea349bc52043659273e20609b5e21a164
tutizao <sant.aert.mar@gmail.com>
2022-02-28 13:49:28 -0300

    first commit

---
+++ Assets/Graphite.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Graphite : MonoBehaviour
+{
+    public float range = 100f;
+
+    public Transform fpsCam;
+    public ParticleSystem Tinta;
+    public GameObject inkSpot;
+
+
+    // Update is called once per frame
+    void Update () {
+    
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            Shoot();
+        }
+        
+    
+    }
+
+    void Shoot ()    
+    {
+        Tinta.Play();
+   
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.position, fpsCam.forward, out hit ,range))
+        {
+            GameObject newTag = Instantiate(inkSpot,hit.point, inkSpot.transform.rotation);
+        }       
+       
+
+       
+    }
+}
